0b7c34c6a30fd2e8f36c89b659d8dbadf217e662
Nagaraju <nagarajumuppavaram2491@gmail.com>
2025-07-16 22:44:28 +0530

    first commit

---
+++ server/Login/index.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+app.use(express.json());
+const port=5000;
+app.get('/',(req,res)=>{
+    res.send("Hello World");
+})
+app.listen(port,(req,res)=>{
+    console.log(`Server is running`);
+})
